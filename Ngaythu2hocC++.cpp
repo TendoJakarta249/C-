3400a184554d50c6ff3992257161a52f819ec87b
TendoJakarta249 <dod35945@gmail.com>
2026-07-10 11:01:20 +0700

    Add files via upload

---
+++ Ngaythu2hocC++.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std; 
+
+int main()
+{
+    for (int i = 1;i <= 7;i++)
+    {
+        for (int j = 1;j <= 7;j++)
+        {
+            if (j=1 && )
+        }
+    }
+}
+
